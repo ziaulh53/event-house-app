802f9db66cbc66d3cad4345af55954fdaf9a03ce
hridoy00005 <hcb2433@gmail.com>
2023-12-09 20:56:29 +0600

    src file updated

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -5,7 +5,6 @@ import { useDispatch } from "react-redux";
 import { notify } from "../utils";
 
 const ForgotPassword = () => {
-
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
@@ -14,19 +13,14 @@ const ForgotPassword = () => {
   });
 
   const onChange = (e) => {
-    const {name, value} = e.target;
+    const { name, value } = e.target;
     setForgotpassword({ ...forgotpassword, email: value });
   };
 
-  const onSubmit = async() => {
+  const onSubmit = async () => {
     try {
       const result = await api.post(auth.forgotPassword, forgotpassword);
-        notify(result);
-        if(result.success){
-          notify(result);
-      }else{
-        notify(result);
-      }
+      notify(result);
     } catch (error) {
       console.log(error);
     }
@@ -42,7 +36,9 @@ const ForgotPassword = () => {
         </div>
         <div className="p-6">
           <div>
-            <label className="text-base md:text-lg font-semibold">Enter Your Email For Verification</label>
+            <label className="text-base md:text-lg font-semibold">
+              Enter Your Email For Verification
+            </label>
             <input
               className="w-full py-2 px-3 border shadow rounded mt-2 focus:outline-none"
               value={forgotpassword.email}
@@ -52,14 +48,14 @@ const ForgotPassword = () => {
               name="email"
             />
           </div>
-          
+
           <div className="mt-5 text-center">
             <button
               className="w-full border p-2 shadow text-lg text-white bg-blue-500 font-semibold rounded"
               type="button"
               onClick={onSubmit}
             >
-              Verification 
+              Verification
             </button>
           </div>
         </div>
